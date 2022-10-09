239c922d92c42789099e5e0e2ac25bca0cbb3faf
Oleh <oleg_bob@ukr.net>
2022-10-09 21:58:29 +0300

    header created

---
+++ src/app/header/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  collapsed: boolean = true;
+  isUserAuth: boolean = false
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onLogout() {
+    this.isUserAuth = false
+  }
+
+  onLogin() {
+    this.isUserAuth = true
+  }
+}
