af1655137117646ef8a16d220f051e707a5a2648
Oleh <oleg_bob@ukr.net>
2022-10-13 14:47:05 +0300

    truncate pipe created

---
+++ src/app/pipes/truncate.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'limitTo'
+})
+export class TruncatePipe implements PipeTransform {
+
+  transform(value: string, args: string) : string {
+    let limit = args ? parseInt(args, 10) : 1000;
+    let trail = '...';
+//якщо строка > за ліміт повертаємо строку довжиною в ліміт + ..., інакше цілу строку
+    return value.length > limit ? value.substring(0, limit) + trail : value;
+  }
+}
