2310542a284068de018218c88fa87a49f7b9644f
Oleh <oleg_bob@ukr.net>
2022-11-01 16:26:10 +0200

    deleted comments

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -25,13 +25,6 @@ export class AuthGuard implements CanActivate {
           }
           return this.router.createUrlTree(['/auth'])
         })
-        //<-----------This is old method---------->
-        // map(user => !!user),
-        // tap(isAuth => {
-        //   if (isAuth) {
-        //     this.router.navigate(['/auth'])
-        //   }
-        // })
       );
   }
 
