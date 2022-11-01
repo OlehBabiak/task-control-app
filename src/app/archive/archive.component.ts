3fd8286a98c00e3fe997d244ddb0d738e00a2cd3
Oleh <oleg_bob@ukr.net>
2022-11-01 16:26:46 +0200

    added logic in comp

--- src/app/archive/archive.component.ts
+++ src/app/archive/archive.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {DataStorageService} from "../shared/data-storage/data-storage.service";
+import {ActivatedRoute,UrlSegment} from "@angular/router";
+import {BoardService} from "../services/board.service";
+import {ColumnTaskModel} from "../shared/column.task-model";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-archive',
   templateUrl: './archive.component.html',
   styleUrls: ['./archive.component.scss']
 })
-export class ArchiveComponent implements OnInit {
+export class ArchiveComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  urlSegment: string
+  tasks: ColumnTaskModel[]
+  private subscription: Subscription
 
-  ngOnInit(): void {
+  constructor(
+    private dataStorage: DataStorageService,
+    private boardService: BoardService,
+    private route: ActivatedRoute
+  ) {
   }
 
+  ngOnInit(): void {
+    this.route.url
+      .subscribe((url: UrlSegment[]) => {
+        this.urlSegment = url[0].path
+      })
+
+    this.dataStorage.getArchiveTask(this.urlSegment)
+      .subscribe({
+        next: (res: ColumnTaskModel[]) => this.boardService.setTask(res),
+        error: (err) => {
+          this.dataStorage.errorSubj.next(err)
+        }
+      })
+
+    this.subscription = this.boardService
+      .tasksChanged
+      .subscribe((tasks: ColumnTaskModel[]) => {
+        console.log(tasks)
+        this.tasks = tasks
+      })
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
+  }
 }
