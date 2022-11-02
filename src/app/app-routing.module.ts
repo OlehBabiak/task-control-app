29763f80c382bd2391f51c033811b83694f7b1c0
Oleh <oleg_bob@ukr.net>
2022-11-02 15:52:05 +0200

    some changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,17 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {AuthComponent} from "./auth/auth.component";
-import {ArchiveComponent} from "./archive/archive.component";
-import {AuthGuard} from "./auth/auth.guard";
+
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: "full"},
-  {
-    path: 'dashboard',
+  {path: 'dashboard',
     loadChildren: () => import('./dashboard/dashboard-routing/dashboard-routing.module')
       .then(module => module.DashboardRoutingModule),
   },
   {path: 'archive',
-    component: ArchiveComponent,
-    canActivate: [AuthGuard],
+    loadChildren: () => import('./archive/archive-routing/archive-routing.module')
+      .then(module => module.ArchiveRoutingModule)
   },
   {path: 'home', component: HomeComponent},
   {path: 'auth', component: AuthComponent}
