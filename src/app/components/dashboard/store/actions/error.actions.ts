638595a9a2f89d96ba6c22b2e2c7552827f38032
Oleh <oleg_bob@ukr.net>
2022-11-25 09:54:15 +0200

    added error action

---
+++ src/app/components/dashboard/store/actions/error.actions.ts
@@ -0,0 +1,13 @@
+import {Action} from "@ngrx/store";
+import {ErrorModel} from "../../../../shared/errors/error-model";
+
+export const SET_ERROR = 'SET_ERROR';
+
+export class SetError implements Action {
+  readonly type = SET_ERROR;
+
+  constructor(public payload: ErrorModel) {
+  }
+}
+
+export type ErrorActions = SetError
