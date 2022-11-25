2b61ba0008b1e3711ef11ecb6bd0ccfe468e39c4
Oleh <oleg_bob@ukr.net>
2022-11-25 09:53:57 +0200

    added error reducer

---
+++ src/app/components/dashboard/store/reducers/error.reducer.ts
@@ -0,0 +1,28 @@
+import * as ErrorActions from "../actions/error.actions";
+import {ErrorModel} from "../../../../shared/errors/error-model";
+
+export interface State {
+  error: ErrorModel
+}
+
+export interface ErrorState {
+  errorItem: State;
+}
+
+const initialState: State = {
+  error: null,
+}
+
+export function errorReducer(
+  state: State = initialState,
+  action: ErrorActions.ErrorActions): State {
+  switch (action.type) {
+    case ErrorActions.SET_ERROR:
+      return {
+        ...state,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+}
